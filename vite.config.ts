354cb3c33b87d0097cf8f7051a2baf7b2f82f902
benknab <git.scz76@passmail.com>
2026-01-09 18:14:21 +0100

    Update build target

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+	environments: {
+		client: {
+			build: {
+				target: "baseline-widely-available",
+			},
+		},
+		ssr: {
+			build: {
+				target: "esnext",
+			},
+		},
+	},
 	plugins: [fresh({ serverEntry: "main.tsx" }), tailwindcss()],
 });
