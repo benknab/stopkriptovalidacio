c2b83ef24010499af63c473808a8fc47c93a6d44
benknab <git.scz76@passmail.com>
2026-01-09 12:27:02 +0100

    Fix plausible script

--- src/components/seo/plausible.tsx
+++ src/components/seo/plausible.tsx
@@ -2,10 +2,17 @@ import type { JSX } from "react";
 
 export function Plausible(): JSX.Element {
 	return (
-		<script
-			defer
-			data-domain="kriptovalidacio.hu"
-			src="https://plausible.io/js/script.js"
-		/>
+		<>
+			<script
+				async
+				src="https://plausible.io/js/pa-ADtpJacMmc0HXCZExXgKR.js"
+			/>
+			<script
+				dangerouslySetInnerHTML={{
+					__html:
+						`window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init();`,
+				}}
+			/>
+		</>
 	);
 }
